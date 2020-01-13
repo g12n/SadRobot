09155791ed0406d6a6acee748f4218822a33047c
mge <m@g12n.de>
2020-01-13 17:46:33 +0100

    include simple css pipeline
    similar to https://github.com/philhawksworth/eleventyone

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,9 @@
 module.exports = {
-    dir: {
-        input: ".",
-        output: "docs"
-    },
-    pathPrefix: "/sad-robot/",
-    templateFormats: [
-        "md",
-        "css", "njk"
-    ],
-    passthroughFileCopy: true
+  dir: {
+    input: ".",
+    output: "docs"
+  },
+  pathPrefix: "/sad-robot/",
+  templateFormats: ["md", "css", "njk", "11ty.js"],
+  passthroughFileCopy: true
 };
