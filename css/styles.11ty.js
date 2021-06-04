03de480d0f1913fe8bd4570b6e6ade8b0c5711fd
Michael Gehrmann <michael.gehrmann@gmail.com>
2021-06-04 08:35:09 +0200

    component and style updates

--- css/styles.11ty.js
+++ css/styles.11ty.js
@@ -1,29 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
 
 // the file name as an entry point for postcss compilation
 // also used to define the output filename in our output /css folder.
-const fileName = "styles.css";
+const fileName = "styles";
 
 module.exports = class {
   async data () {
-    const rawFilepath = path.join(__dirname, `../_includes/postcss/${fileName}`);
+    const rawFilepath = path.join(__dirname, `./${fileName}.scss`);
     return {
-      permalink: `css/${fileName}`,
+      permalink: `css/${fileName}.css`,
       rawFilepath,
       rawCss: await fs.readFileSync(rawFilepath)
     };
   };
-
   async render ({ rawCss, rawFilepath }) {
     return await postcss([
       // require('postcss-comment'),
       require('precss'),
       require('postcss-import'),
       require('postcss-mixins'),
-      require('postcss-color-mix'),
-      require('cssnano')
+      require('cssnano'),
+      autoprefixer
     ])
     .process(rawCss, { from: rawFilepath })
     .then(result => result.css);
